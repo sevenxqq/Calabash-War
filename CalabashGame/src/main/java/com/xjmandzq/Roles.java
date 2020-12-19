b61c1cc6f776088747b54ba2411d0f69192cb9c7
sevenxqq <fantastic772256@163.com>
2020-12-19 11:33:05 +0800

    roles frame v1

---
+++ CalabashGame/src/main/java/com/xjmandzq/Roles.java
@@ -0,0 +1,33 @@
+
+
+package com.xjmandzq;
+
+//----------葫芦阵营-------------
+class Calabash extends Creature{
+
+}
+
+class Grandpa extends Creature{
+
+}
+
+class Pangolin extends Creature{
+
+}
+
+//-----------------妖怪阵营---------
+
+class Snake extends Creature{
+
+}
+
+
+class Scorpion extends Creature{
+
+}
+
+
+class Frog extends Creature{
+
+}
+
